aeb2f42776d38f2d1f1c8e69d94896ec8843b088
Anthony <thonywong@rocketmail.com>
2016-11-20 18:19:54 +0700

    Multi testcase format

--- punch/gen-hard.cpp
+++ punch/gen-hard.cpp
@@ -2,31 +2,37 @@
 
 using namespace std;
 
-const int N = 100000;
+const int MAXN = 100000;
 const int SQ = 505;
 
 const int MUL = 2147483647 / 505;
 const int TEMP = MUL / 505;
 
 int main() {
-	printf("%d\n", N);
-	vector<pair<int, int> > v;
-	v.push_back({-2147483647, -2147483647});
-	v.push_back({-2147483648, -2147483648});
-	
-	int X = MUL, Y = TEMP;
-	for (int i = 0; i <= N / SQ; ++i) {
-		for (int j = 0; j < SQ && (int) v.size() < N; ++j) {
-			v.push_back({rand() % X, rand() % TEMP});
+	int tc = 10;
+	printf("%d\n", tc);
+	for (int t = 0; t < tc; ++t) {
+		int N = rand() % (MAXN >> 1) + 1 + (MAXN >> 1);
+		assert((MAXN >> 1) <= N && N <= MAXN);
+		printf("%d\n", N);
+		vector<pair<int, int> > v;
+		v.push_back({-2147483647, -2147483647});
+		v.push_back({-2147483648, -2147483648});
+		
+		int X = MUL, Y = TEMP;
+		for (int i = 0; i <= N / SQ; ++i) {
+			for (int j = 0; j < SQ && (int) v.size() < N; ++j) {
+				v.push_back({rand() % X, rand() % TEMP});
+			}
+			X += MUL;
+			Y += TEMP;
 		}
-		X += MUL;
-		Y += TEMP;
-	}
 
-	random_shuffle(v.begin(), v.end());
+		random_shuffle(v.begin(), v.end());
 
-	for (int i = 0; i < N; ++i) {
-		printf("%d %d\n", v[i].first, v[i].second);
+		for (int i = 0; i < N; ++i) {
+			printf("%d %d\n", v[i].first, v[i].second);
+		}
 	}
 	return 0;
 }
--- punch/solution.cpp
+++ punch/solution.cpp
@@ -2,18 +2,21 @@
 
 using namespace std;
 
-set<long long> sx, sy, sxminy, sxplusy;
-
 int main() {
 	int n, x, y;
-	scanf("%d", &n);
-	for (int i = 0; i < n; ++i) {
-		scanf("%d %d", &x, &y);
-		sx.insert(x);
-		sy.insert(y);
-		sxminy.insert((long long) x - y);
-		sxplusy.insert((long long) x + y);
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		scanf("%d", &n);
+		set<long long> sx, sy, sxminy, sxplusy;
+		for (int i = 0; i < n; ++i) {
+			scanf("%d %d", &x, &y);
+			sx.insert(x);
+			sy.insert(y);
+			sxminy.insert((long long) x - y);
+			sxplusy.insert((long long) x + y);
+		}
+		printf("%d\n", (int) (sx.size() + sy.size() + sxminy.size() + sxplusy.size()));
 	}
-	printf("%d\n", (int) (sx.size() + sy.size() + sxminy.size() + sxplusy.size()));
 	return 0;
 }
