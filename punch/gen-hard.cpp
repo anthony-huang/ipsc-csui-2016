9fa19a22049a87b9ba01dcbe65e25ea8870eb2a3
Anthony <thonywong@rocketmail.com>
2016-11-23 16:12:11 +0700

    Added new tc

--- punch/gen-hard.cpp
+++ punch/gen-hard.cpp
@@ -11,7 +11,7 @@ const int TEMP = MUL / 505;
 int main() {
 	int tc = 10;
 	printf("%d\n", tc);
-	for (int t = 0; t < tc; ++t) {
+	for (int t = 0; t < 8; ++t) {
 		int N = rand() % (MAXN >> 1) + 1 + (MAXN >> 1);
 		assert((MAXN >> 1) <= N && N <= MAXN);
 		printf("%d\n", N);
@@ -22,7 +22,12 @@ int main() {
 		int X = MUL, Y = TEMP;
 		for (int i = 0; i <= N / SQ; ++i) {
 			for (int j = 0; j < SQ && (int) v.size() < N; ++j) {
-				v.push_back({rand() % X, rand() % TEMP});
+				int x = rand() % X, y = rand() % TEMP;
+				if (rand() % 2)
+					x = -x;
+				if (rand() % 2)
+					y = -y;
+				v.push_back({x, y});
 			}
 			X += MUL;
 			Y += TEMP;
@@ -30,6 +35,28 @@ int main() {
 
 		random_shuffle(v.begin(), v.end());
 
+		for (int i = 0; i < N; ++i) {
+			printf("%d %d\n", v[i].first, v[i].second);
+		}
+	}
+	int num[] = {-2147483648, 2147483647};
+	int val[] = {+1, -1};
+	for (int t = 0; t < 2; ++t) {
+		int N = rand() % (MAXN >> 1) + 1 + (MAXN >> 1);
+		printf("%d\n", N);
+		vector<pair<int, int> > v;
+		int X = MUL, Y = TEMP;
+		for (int i = 0; i <= N / SQ; ++i) {
+			for (int j = 0; j < SQ && (int) v.size() < N; ++j) {
+				int a = rand() % 2, b = rand() % 2;
+				int x = num[a] + (rand() % X) * val[a], y = num[b] + (rand() % TEMP) * val[b];
+				v.push_back({x, y});
+			}
+			X += MUL;
+			Y += TEMP;
+		}
+
+		random_shuffle(v.begin(), v.end());
 		for (int i = 0; i < N; ++i) {
 			printf("%d %d\n", v[i].first, v[i].second);
 		}
